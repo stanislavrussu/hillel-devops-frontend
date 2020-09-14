241e0be3e0202708af0dc0630db878351cf7ceea
stanislavrussu <stanislavrussu92@gmail.com>
2020-09-14 21:39:33 +0300

    second commit

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'http://backend-1809598107.us-east-1.elb.amazonaws.com/api';
+const API_ROOT = 'BACKEND_ENDPOINT_VARIABLE';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
